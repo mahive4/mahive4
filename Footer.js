896f0d8e8960038b62ec2350fb9f42e62a11a43b
Vaibhav <83857082+vaibhav1511ux@users.noreply.github.com>
2021-09-06 14:03:15 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render() {
+return(
+        <Footer>
+            <p>COnfigure Your car</p>
+            <p>privacy|terms|Condistions</p>
+        </Footer>
+        );
+    }
+
+}
+
+
+export default Footer;
